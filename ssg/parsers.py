ee0206e823a47e038d9c9738210118a126df4ef3
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 15:36:47 +0100

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class Parser(object):
     """docstring for Parser."""
-    extensions = List[str]
+    extensions: List[str] = []
 
     def __init__(self, arg):
         super(Parser, self).__init__()
