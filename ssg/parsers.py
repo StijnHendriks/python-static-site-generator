ca42b78323096b5e4ff679e38802684b210619cf
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 16:09:24 +0100

    .

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,10 +7,6 @@ class Parser(object):
     """docstring for Parser."""
     extensions: List[str] = []
 
-    def __init__(self, arg):
-        super(Parser, self).__init__()
-        self.arg = arg
-
     def valid_extension(self, extension):
         return extension in self.extensions
 
