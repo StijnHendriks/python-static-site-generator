f28dfdebfb0c3ea6b22391c65f8bee95cce168fd
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 16:40:13 +0100

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -4,7 +4,7 @@ from collections.abc import Mapping
 
 class Content(Mapping):
     """docstring for Content."""
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
     def __init__(self, arg):
