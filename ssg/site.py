62215d532843594064a269aaee3499fae79338d5
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 16:31:23 +0100

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -17,7 +17,8 @@ class Site(object):
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-            elif self.run_parser(path)
+            elif path.is_file():
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
