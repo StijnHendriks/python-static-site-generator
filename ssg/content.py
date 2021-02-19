ee6d76af3ba8b1b4b82d75aef6772c9e722c0949
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 16:34:37 +0100

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -1,3 +1,11 @@
 import re
 from yaml import load, FullLoader
 from collections.abc import Mapping
+
+class Content(Mapping):
+    """docstring for Content."""
+    __regix = re.compile(__delimiter, re.MULTILINE)
+
+    def __init__(self, arg):
+        super(Content, self).__init__()
+        self.arg = arg
