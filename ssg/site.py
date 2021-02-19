82036ddd968eacbc840703f025211f06f44d1cb1
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 15:16:45 +0100

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -6,3 +6,6 @@ class Site(object):
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / Path.relative_to(slef.source)
