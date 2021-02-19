f655e635c2899dfdfd4cb3f26fb8c1e4b3c7eb18
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 15:55:53 +0100

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -28,3 +28,10 @@ class Parser(object):
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+    """docstring for ResourceParser."""
+    extensions : List[str] = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
