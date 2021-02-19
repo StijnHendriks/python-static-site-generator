9a1c5ee2065406bb294db352601f99c72eb1a6dd
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 15:48:13 +0100

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -21,4 +21,5 @@ class Parser(object):
 
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        
+        with open(full_path, 'w') as file:
+            file.write(content)
