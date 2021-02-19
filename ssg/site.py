e8a223fed57838e7197356347b2eea63fb7c138d
Stijn-Hendriks <stijn.hendriks@gmail.com>
2021-02-19 16:29:26 +0100

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -27,6 +27,6 @@ class Site(object):
         parser = self.load_parser(path.suffix)
 
         if(parser is not None):
-            parser.parse(path, path, path)
+            parser.parse(path, self.source, self.dest)
         else:
-            raise NotImplementedError
+            print("Not Implemented")
